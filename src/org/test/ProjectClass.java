9785dceb880cd14570bd38fb4be9e44047c83ba7
PrakashRcg21 <prakashrcg21@gmail.com>
2021-09-10 23:31:49 +0530

    loadUrl

--- src/org/test/ProjectClass.java
+++ src/org/test/ProjectClass.java
@@ -2,4 +2,12 @@ package org.test;
 
 public class ProjectClass {
 
+	public static void loadurl() {
+		System.out.println("loadurl");
+	}
+	public static void main(String[] args) {
+		loadurl();
+	}
+	
+	
 }
