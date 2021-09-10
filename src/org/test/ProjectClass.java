7097b904a714b574f9d761f127ea35686e1a5ebc
PrakashRcg21 <prakashrcg21@gmail.com>
2021-09-10 23:20:12 +0530

    getDriver

--- src/org/test/ProjectClass.java
+++ src/org/test/ProjectClass.java
@@ -2,4 +2,10 @@ package org.test;
 
 public class ProjectClass {
 
+	public  static void getDriver() {
+		System.out.println("getDriver");
+	}
+	public static void main(String[] args) {
+		getDriver();
+	}
 }
